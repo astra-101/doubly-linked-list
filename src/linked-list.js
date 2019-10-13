ba8cced38f68f026ce539eff10c94b7d8c6ff66a
Olga_Sokolova <olia040889@gmail.com>
2019-10-13 17:38:16 +0300

    feat: implement functions to work with doubly linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,161 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+      this._tail = null;
+      this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+        let node = new Node(data)
+        if( this._tail===null) {
+            this._head = node;
+            this._tail = node;
+            this.length++;
 
-    head() {}
+        }
+        else{
+            node.next = null;
+            node.prev = this._tail;
+            this._tail.next = node;
+            this._tail = node;
+            this.length++;
+        }
+        return this;
+    }
 
-    tail() {}
+    head() {
+        if (this._head === null) {
+            return null
+        }
+        else{
+            return this._head.data
+        }
+     }
+    
 
-    at(index) {}
+    tail() {
+        if (this._tail === null) {
+            return null
+        }
+        else{
+        return this._tail.data
+    }
+    }
 
-    insertAt(index, data) {}
+    at(index) { 
+        let NodeYouNeed = this._head;
 
-    isEmpty() {}
+        for(var i=0; i<this.length; i++){
+                      
+            if (i === index){
+                return NodeYouNeed.data;
+            }
 
-    clear() {}
+            NodeYouNeed = NodeYouNeed.next;
+        }
 
-    deleteAt(index) {}
+    }
 
-    reverse() {}
+    insertAt(index, data) {
+        let node = new Node(data)
+        let NodeYouChange = this._head;
+        for(var i=0; i<this.length; i++){
+            if (i === index){
+                if (i===0) {
+                    node.prev = null;
+                    node.next = this._head;
+                    this._head.prev = node;
+                    this._head = node;
+                    this.length++;
+                    break;
+                }
+                else{          
+                    node.prev = NodeYouChange.prev;   
+                    node.next = NodeYouChange;
+                    NodeYouChange.prev = node;
+                    node.prev.next = node;
+                    this.length++; 
+                    break;
+                }
+            }
+            NodeYouChange = NodeYouChange.next;
 
-    indexOf(data) {}
+        }
+        return this;
+    }
+    isEmpty() {
+
+        if (this._head === null) {
+            return true
+        }
+        else {return false}
+
+    }
+
+    clear() {
+       
+        this._head = null;
+        this._tail = null;
+        this.length =0;
+        return this;
+    }
+
+    deleteAt(index) {
+        let NodeYouDelete = this._head;
+        let NodeYouDeleted = null;
+        for(var i=0; i<this.length; i++){
+            
+            if (i === index){
+               if(i===0){
+                NodeYouDelete.next = this._head;
+                NodeYouDelete = NodeYouDeleted;
+                break;
+                }
+                if(i === this.length-1){
+                NodeYouDelete.prev = this._tail;
+                NodeYouDelete = NodeYouDeleted;
+                break;
+                }
+                else{
+                NodeYouDelete.prev.next=NodeYouDelete.next;
+                NodeYouDelete.next.prev=NodeYouDelete.prev;
+                NodeYouDelete = NodeYouDeleted;
+                break;
+                }
+        }
+            NodeYouDelete = NodeYouDelete.next;
+        }
+        return this;
+    }
+
+    reverse() {
+        let NodeStart = this._head;
+        let NodeEnd = this._tail;
+        let node;
+        for(var i=0; i<this.length/2; i++){
+            node = NodeEnd.data;
+            NodeEnd.data = NodeStart.data;
+            NodeStart.data = node;
+            NodeStart = NodeStart.next;
+            NodeEnd = NodeEnd.prev;
+        }
+        return this;
+    }
+
+    indexOf(data) {
+        let NodeYouNeed = this._head;
+        for (var i=0; i<this.length; i++) {
+            
+            if (NodeYouNeed.data === data){
+                return i;
+            }
+
+            NodeYouNeed = NodeYouNeed.next;
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
